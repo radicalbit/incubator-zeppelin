f1f57687aee9481d86c56bec3a2a679f32b9b48f
simone <simone.robutti@databiz.it>
2016-03-29 17:25:35 +0200

    Scala-2.11 build working

--- cassandra/src/test/java/org/apache/zeppelin/cassandra/InterpreterLogicTest.java
+++ cassandra/src/test/java/org/apache/zeppelin/cassandra/InterpreterLogicTest.java
@@ -302,10 +302,10 @@ public class InterpreterLogicTest {
     }
 
     private <A> scala.collection.immutable.List<A> toScalaList(java.util.List<A> list)  {
-        return scala.collection.JavaConversions.asScalaIterable(list).toList();
+        return scala.collection.JavaConversions.iterableAsScalaIterable(list).toList();
     }
 
     private  <A> java.util.List<A> toJavaList(scala.collection.immutable.List<A> list){
-        return scala.collection.JavaConversions.asJavaList(list);
+        return scala.collection.JavaConversions.seqAsJavaList(list);
     }
 }
--- flink/src/main/java/org/apache/zeppelin/flink/FlinkInterpreter.java
+++ flink/src/main/java/org/apache/zeppelin/flink/FlinkInterpreter.java
@@ -47,6 +47,7 @@ import scala.Some;
 import scala.runtime.AbstractFunction0;
 import scala.tools.nsc.Settings;
 import scala.tools.nsc.interpreter.IMain;
+import scala.tools.nsc.interpreter.NamedParamClass;
 import scala.tools.nsc.interpreter.Results;
 import scala.tools.nsc.settings.MutableSettings.BooleanSetting;
 import scala.tools.nsc.settings.MutableSettings.PathSetting;
@@ -115,7 +116,8 @@ public class FlinkInterpreter extends Interpreter {
     
     imain.interpret("import org.apache.flink.api.scala._");
     imain.interpret("import org.apache.flink.api.common.functions._");
-    imain.bindValue("env", env);
+    imain.bind(new NamedParamClass("env",
+            "org.apache.flink.api.scala.ExecutionEnvironment", env));
   }
 
   private boolean localMode() {
